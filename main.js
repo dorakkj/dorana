4fbdd9f6265a80e874be064e7728f55563664882
dorakkj <doravitoria77@gmail.com>
2024-09-27 08:51:57 -0300

    .

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 function soma(num1, num2) {
-    return num1+num2;
+return num1+num2;
 }
 console.log(soma(5,4))
 function mult(num1,num2){
 return num1*num2;
+}
+function areaQuarado(lado){
+return lado**2;
 }
