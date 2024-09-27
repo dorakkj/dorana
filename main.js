2ea4730a4613c1ca6ddc65c9d66c4709bc063e04
dorakkj <doravitoria77@gmail.com>
2024-09-27 09:26:58 -0300

    b

--- main.js
+++ main.js
@@ -9,10 +9,11 @@ function areaQuadrado(lado){
 return lado**2;
 }
 console.log (areaQuadrado(6));
+let nome=parseint(prompt('qual o valor?'));
+
 let media=function (x,y,z){
     return(1+2+3)/3;
 }
 console.log(media(1+2+3/3));
-let nome=parseint(prompt('qual o valor?'));
-console.log('qual o valor?')
+
 
