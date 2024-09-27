49f57ccd444d80ecfaa0eb288f5f3d35ea3f2a67
dorakkj <doravitoria77@gmail.com>
2024-09-27 09:18:14 -0300

    c

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function areaQuadrado(lado){
 return lado**2;
 }
 console.log (areaQuadrado(6));
-let media=function (media){
+let media=function (x,y,z){
     return(1+2+3)/3;
 }
 console.log(media(1+2+3/3));
