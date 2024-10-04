0a7db536b15f352468f8ac891151983d06b58e8a
dorakkj <doravitoria77@gmail.com>
2024-10-04 08:32:06 -0300

    M

--- main.js
+++ main.js
@@ -14,6 +14,6 @@
 //     return(1+2+3)/3;
 // }
 // console.log(media(1+2+3/3));
-let estados=["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PA","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO",]
+let estados=["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PA","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO",];
 
 
