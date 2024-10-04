ed68ab094947d2e62c840697dc150c3ba0fdccc0
dorakkj <doravitoria77@gmail.com>
2024-10-04 09:17:13 -0300

    c

--- main.js
+++ main.js
@@ -16,10 +16,14 @@
 // console.log(media(1+2+3/3));
 let estados=["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PR","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO",];
 
-let i=0;
-while(i <27){
-console.log(i);
-console.log(estados[i]);
-i++;
-}
+// let i=0;
+// while(i <27){
+// console.log(i);
+// console.log(estados[i]);
+// i++;
+// }
 
+for(let j=0; j<estados.length; j++){
+console.log(j);
+console.log(estados[j]);
+}
