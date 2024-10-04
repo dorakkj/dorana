4f847dcb632b2268fc0fb1a52fbb016134f02015
dorakkj <doravitoria77@gmail.com>
2024-10-04 08:28:34 -0300

    C

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
-function soma(num1, num2) {
-return num1+num2;
-}
-console.log(soma(5,4));
-function mult(num1,num2){
-return num1*num2;
-}
-function areaQuadrado(lado){
-return lado**2;
-}
-console.log (areaQuadrado(6));
-let nome=valoresMedia=(prompt("qual sua nota do primeiro trimestre?"));
-let media=function(x,y,z){
-    return(1+2+3)/3;
-}
-console.log(media(1+2+3/3));
+// function soma(num1, num2) {
+// return num1+num2;
+// }
+// console.log(soma(5,4));
+// function mult(num1,num2){
+// return num1*num2;
+// }
+// function areaQuadrado(lado){
+// return lado**2;
+// }
+// console.log (areaQuadrado(6));
+// let nome=valoresMedia=(prompt("qual sua nota do primeiro trimestre?"));
+// let media=function(x,y,z){
+//     return(1+2+3)/3;
+// }
+// console.log(media(1+2+3/3));
+let estados=["AC","AL","AP","AM","BA","CE","DF","ES","GO","MA","MT","MS","MG","PA","PB","PA","PE","PI","RJ","RN","RS","RO","RR","SC","SP","SE","TO",]
+
+
